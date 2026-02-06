3b7e49d7981f7464f5e82b35105e18a0635f7532
Saksham Srivastava <saksham17126srivastava@gmail.com>
2026-02-06 13:50:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import pyautogui 
+while True:
+    a=pyautogui.position()
+    print(a)
+    # 1338,975
+    # window icon--697,1045
+    # one tap to deselect->1755,896
+    # for inpur filed click-->1266,975
+    
